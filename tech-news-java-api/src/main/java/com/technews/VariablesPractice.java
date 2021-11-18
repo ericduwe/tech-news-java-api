6c920f6231557b09d2aaf71e9325187983cfe7d3
Eric Duwe <ericduwe@gmail.com>
2021-11-18 17:05:28 -0600

    added User model

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int numone = 3;
+        double numtwo = 3.5;
+        String word = "doggo";
+
+        String numword = word + numone;
+
+        double sum = numone + numtwo;
+
+        System.out.println(sum);
+        System.out.println(numword);
+
+    }
+}
